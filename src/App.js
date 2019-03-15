a4e15e8d3de12f0dd116f1041d38fe504286d279
RonaldVilorio <ronvilorio@gmail.com>
2019-03-15 19:01:08 -0400

    added Navbar component and added routes to it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-
-import AppRouter from "./routes";
+import NavBar from "./components/NavBar";
 class App extends Component {
   render() {
     return (
       <div>
-        <AppRouter />
+        <NavBar />
       </div>
     );
   }
---
+++ src/components/NavBar.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import AppRouter from "../routes";
+class NavBar extends Component {
+  render() {
+    return <AppRouter />;
+  }
+}
+export default NavBar;
